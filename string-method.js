2d9127aabe3246be14234270416e0a9eb0a5fd87
manoj1202 <manoj.appalaneni@gmail.com>
2018-04-29 10:55:34 -0500

    adding code

--- string-method.js
+++ string-method.js
@@ -11,3 +11,15 @@ let password = 'askdnlnpassword';
 console.log(password.includes('password'))
 
 console.log(name.trim());
+
+let isValidPassword = function(password){
+    if(password.length > 8 && !password.includes('password')){
+        return True
+    } else {
+        return False
+    }
+}
+
+console.log(isValidPassword('hbfbh3'))
+console.log(isValidPassword('afh38983enedwiu'))
+console.log(isValidPassword('bfhb3wir7Password'))
