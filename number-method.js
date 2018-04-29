ffa681ec2e52a18f641e79589632ea875f0b59a5
manoj1202 <manoj.appalaneni@gmail.com>
2018-04-29 12:33:37 -0500

    Adding data

--- number-method.js
+++ number-method.js
@@ -6,7 +6,14 @@ console.log(Math.round(num));
 console.log(Math.ceil(num));
 console.log(Math.floor(num));
 // rondom number generating
-let min = 10;
+let min = 0;
 let max =  200;
 let random = Math.floor(Math.random()*(max -min) + min);
 console.log(random);
+
+//challenge
+let makeGuess = functoion(num1){
+    let min = 1;
+    let max = 5;
+    let random = Math.floor(Math.random()*(max-min+1));
+}
